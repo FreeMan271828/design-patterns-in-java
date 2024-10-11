0091dc6bf0f46fd3befebedd933c940660f102fa
黄施远 <120921289+FreeMan271828@users.noreply.github.com>
2024-10-11 14:06:40 +0800

    Update Test.java

--- structual/bridge/src/test/java/Test.java
+++ structual/bridge/src/test/java/Test.java
@@ -9,7 +9,7 @@ public class Test {
 
     @org.junit.jupiter.api.Test
     public void test() {
-        // 先构建出实现化部分,也就是口味
+        // 先构建出实现化部分,也就是本质
         // 由于Coffee采用抽象类, 所以可以再增加一个维度(大杯,小杯...)
         Coffee coffee = new LargeCoffee(new Milk());
         // 再使用抽象化部分,选择点了多少杯
